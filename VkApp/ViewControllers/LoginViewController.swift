6bec52f5432542850b5137f04310384ea0c3618e
Karim <karimruabhi@MacBook-Pro-Karim.local>
2021-12-17 10:37:55 +0300

    VkApp v.1.1

--- VkApp/ViewControllers/LoginViewController.swift
+++ VkApp/ViewControllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var userPasswordTextField: UITextField!
