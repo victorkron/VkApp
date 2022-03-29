2d0bd27430a3693aa48537f4fe22d6a3a21957f2
Karim <k.kron2015@yandex.ru>
2022-03-29 15:40:15 +0300

    v.3.1.1 news pattern

--- VkApp/Models/News.swift
+++ VkApp/Models/News.swift
@@ -8,39 +8,6 @@
 import Foundation
 import UIKit
 
-protocol PropertyLoopable
-{
-    func allProperties() throws -> [String: Any]
-}
-
-extension PropertyLoopable
-{
-    func allProperties() throws -> [String: Any] {
-
-        var result: [String: Any] = [:]
-
-        let mirror = Mirror(reflecting: self)
-
-        guard
-            let style = mirror.displayStyle,
-            style == .struct || style == .class
-        else {
-            //throw some error
-            throw NSError(domain: "hris.to", code: 777, userInfo: nil)
-        }
-
-        for (labelMaybe, valueMaybe) in mirror.children {
-            guard let label = labelMaybe else {
-                continue
-            }
-            
-            result[label] = valueMaybe
-        }
-
-        return result
-    }
-}
-
 struct Title {
     var avatar: UIImage?
     var name: String
@@ -74,7 +41,7 @@ struct ActionsData {
 
 
 
-class News: PropertyLoopable {
+class News {
     
     var title: Title?
     var description: String?
--- VkApp/View/Reusable Cells/NewsTableCells/AvatarCell.swift
+++ VkApp/View/Reusable Cells/NewsTableCells/AvatarCell.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import SwiftUI
 
 class AvatarCell: UITableViewCell {
     
     @IBOutlet var avatar: UIImageView!
     @IBOutlet var name: UILabel!
-
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBAction func buttonHasPressed(_ sender: Any) {
+        print("Hello")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -23,11 +29,11 @@ class AvatarCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    func configure(_ image: UIImage?, _ name: String) {
+    func configure(_ image: UIImage?, _ name: String, _ date: String) {
         self.avatar.frame.size = CGSize(width: 50, height: 50)
         self.avatar.image = image
         self.name.text = name
-        
+        self.dateLabel.text = date
     }
     
 }
